e7cdec86babefb72c94aeed466d4b40e625eb0ee
Oscar <avilaoe@miamioh.edu>
2025-03-31 22:29:40 -0400

    frontend bt test

--- Safe-Step-App/src/pages/Home.jsx
+++ Safe-Step-App/src/pages/Home.jsx
@@ -1,25 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 
 function Home() {
-  const [btAllowed, setBtAllowed] = useState(false);
+  const [btDevice, setBTDevice] = useState({});
+  // A07498CA-AD5B474E-940D-16F1FBE7E8CD
 
-  useEffect(() => {
-    const checkBluetoothPermission = async () => {
-      const btPermission = await navigator.permissions.query({ name: "bluetooth" });
-      if (btPermission.state !== "denied") {
-        setBtAllowed(true);
-      }
-    };
+  const connectToSafeStep = () => {
+    console.log("Connecting to Safe Step...");
 
-    checkBluetoothPermission();
-  }, []);
+    navigator.bluetooth.requestDevice({
+      filters: [{
+      services: ['a07498ca-ad5b-474e-940d-16f1fbe7e8cd']
+      }]
+    })
+    .then(device => { setBTDevice(device); console.log(device); setBTConnected(true); return device.gatt.connect(); })
+    .catch(error => { console.error(error); });
+  }
 
   return (
     <div className="flex flex-col h-screen justify-between">
       <main className="mb-auto mx-2">
-        Home
-        {btAllowed && <p>Bluetooth is allowed on this device.</p>}
+        <button type="button" onClick={connectToSafeStep} className="border border-primary rounded-xs">Click here to connect to Safe Step</button>
+        {btDevice && btDevice.name}
       </main>
 
       <footer><NavBar /></footer>
