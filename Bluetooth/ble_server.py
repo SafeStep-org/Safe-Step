bc70b1777e87765b2f00af22057ce3f98d90192a
Oscar <avilaoe@miamioh.edu>
2025-04-10 20:58:46 -0400

    bt server changes

--- Bluetooth/ble_server.py
+++ Bluetooth/ble_server.py
@@ -1,9 +1,10 @@
+# safe_pi_ble_server.py
+
 import sys
-import logging
 import asyncio
 import threading
-from typing import Any, Union
-
+import logging
+from typing import Union, Optional
 from bless import (
     BlessServer,
     BlessGATTCharacteristic,
@@ -14,81 +15,61 @@ from bless import (
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-class BLEServer:
+class SafePiBLEServer:
     def __init__(self, loop: asyncio.AbstractEventLoop):
         self.loop = loop
-        self.server = None
         self.trigger: Union[asyncio.Event, threading.Event]
+        self.trigger = threading.Event() if sys.platform in ["darwin", "win32"] else asyncio.Event()
 
-        if sys.platform in ["darwin", "win32"]:
-            self.trigger = threading.Event()
-        else:
-            self.trigger = asyncio.Event()
-
-        self.service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
-        self.char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-
-    def read_request(self, characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
-        logger.debug(f"Reading value: {characteristic.value}")
-        return characteristic.value
-
-    def write_request(self, characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
-        characteristic.value = value
-        logger.debug(f"Received write: {value}")
-        if value == b"\x0f":
-            logger.debug("Trigger value received!")
-            self.trigger.set()
-
-    async def start(self):
-        self.trigger.clear()
-        self.server = BlessServer(name="Safe-Pi", loop=self.loop)
+        self.server = BlessServer(name="Safe-Pi", loop=loop)
         self.server.read_request_func = self.read_request
         self.server.write_request_func = self.write_request
 
-        # Add service and characteristic
+        self.service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
+        self.char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+        self.characteristic: Optional[BlessGATTCharacteristic] = None
+
+    async def start(self):
         await self.server.add_new_service(self.service_uuid)
 
-        props = (
+        char_flags = (
             GATTCharacteristicProperties.read
             | GATTCharacteristicProperties.write
             | GATTCharacteristicProperties.indicate
         )
-        perms = (
+        permissions = (
             GATTAttributePermissions.readable
             | GATTAttributePermissions.writeable
         )
-
         await self.server.add_new_characteristic(
-            self.service_uuid,
-            self.char_uuid,
-            props,
-            None,
-            perms,
+            self.service_uuid, self.char_uuid, char_flags, None, permissions
         )
 
+        self.characteristic = self.server.get_characteristic(self.char_uuid)
         await self.server.start()
-        logger.info("BLE server started and advertising")
-
-        if isinstance(self.trigger, threading.Event):
-            self.trigger.wait()
-        else:
-            await self.trigger.wait()
-
-    async def send(self, data: str):
-        if self.server is None:
-            logger.warning("BLE server not started.")
-            return
-
-        char = self.server.get_characteristic(self.char_uuid)
-        if char is None:
-            logger.warning("Characteristic not found.")
-            return
-
-        encoded = data.encode("utf-8")
-        logger.debug(f"Sending: {encoded}")
-        self.server.update_value(self.service_uuid, self.char_uuid, encoded)
+        logger.info("BLE server started and advertising.")
 
     async def stop(self):
-        if self.server:
-            await self.server.stop()
-            logger.info("BLE server stopped.")
+        await self.server.stop()
+        logger.info("BLE server stopped.")
+
+    def read_request(self, characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
+        logger.debug(f"Reading {characteristic.value}")
+        return characteristic.value
+
+    def write_request(self, characteristic: BlessGATTCharacteristic, value: bytearray, **kwargs):
+        message = value.decode('utf-8')
+        logger.info(f"Received from client: {message}")
+        if self.characteristic:
+            self.server.update_value(self.service_uuid, self.char_uuid, b"hello from server")
+        if self.trigger.__module__ == "threading":
+            self.trigger.set()
+        else:
+            self.loop.call_soon_threadsafe(self.trigger.set)
+
+    async def send_message(self, msg: str):
+        if self.characteristic:
+            self.server.update_value(self.service_uuid, self.char_uuid, msg.encode('utf-8'))
+            logger.info(f"Sent to client: {msg}")
+        else:
+            logger.warning("No client connected; message not sent.")
