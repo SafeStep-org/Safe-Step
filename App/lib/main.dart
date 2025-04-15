f0c5e209f7940111fb56610c2463ebbed2a8e330
Oscar <avilaoe@miamioh.edu>
2025-04-15 11:20:07 -0400

    added flutter app

---
+++ App/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'map.dart';
+import 'home.dart';
+import 'settings.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Safe Step",
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    Home(),
+    Map(),
+    Settings(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Safe Step'),
+        backgroundColor: Theme.of(context).highlightColor,
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
