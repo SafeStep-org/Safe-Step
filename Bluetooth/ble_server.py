6b667fee450ad9cc16c99649e42429349d02bd26
= <=>
2025-04-15 14:15:29 -0400

    2 way works!

--- Bluetooth/ble_server.py
+++ Bluetooth/ble_server.py
@@ -21,12 +21,12 @@ class SafePiBLEServer:
         self.trigger: Union[asyncio.Event, threading.Event]
         self.trigger = threading.Event() if sys.platform in ["darwin", "win32"] else asyncio.Event()
 
-        self.server = BlessServer(name="Safe-Pi", loop=loop)
+        self.server = BlessServer(name="Safe-Step", loop=loop)
         self.server.read_request_func = self.read_request
         self.server.write_request_func = self.write_request
 
         self.service_uuid = "302c754d-63c1-4c28-a5ff-ad3e9f332226"
-        self.char_uuid = "3a98b215-2971-4c6d-b5c2-02597ae99d0e"
+        self.char_uuid = "3A98B215-2971-4C6D-B5C2-02597AE99D0E"
         self.characteristic: Optional[BlessGATTCharacteristic] = None
 
     async def start(self):
@@ -37,6 +37,7 @@ class SafePiBLEServer:
             | GATTCharacteristicProperties.write
             | GATTCharacteristicProperties.indicate
             | GATTCharacteristicProperties.write_without_response
+            | GATTCharacteristicProperties.notify
         )
         permissions = (
             GATTAttributePermissions.readable
@@ -61,7 +62,9 @@ class SafePiBLEServer:
     def write_request(self, characteristic: BlessGATTCharacteristic, value: bytearray, **kwargs):
         message = value.decode('utf-8')
         logger.info(f"Received from client: {message}")
-        self.characteristic.value = message
+        
+        self.characteristic.value = value
+        self.server.update_value(self.service_uuid, self.char_uuid)
         
         if self.trigger .__module__ == "threading":
             self.trigger.set()
@@ -71,6 +74,7 @@ class SafePiBLEServer:
     async def send_message(self, msg: str):
         if self.characteristic:
             self.characteristic.value = msg.encode('utf-8')
+            self.server.update_value(self.service_uuid, self.char_uuid)
             logger.info(f"Sent to client: {msg}")
         else:
             logger.warning("No client connected; message not sent.")
