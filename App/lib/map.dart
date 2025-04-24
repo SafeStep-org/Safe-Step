bb4267f690c64945378bee81860c7b0393407d5d
sweetmatt <113383848+sweetmatt@users.noreply.github.com>
2025-04-23 21:55:05 -0400

    Update map.dart

--- App/lib/map.dart
+++ App/lib/map.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class Map extends StatelessWidget {
-  const Map({super.key});
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Map'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Accessible Routes')),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(39.5073, -84.7452), // Oxford, Ohio
+          zoom: 15.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.accessible_routes_app',
+          ),
+        ],
+      ),
     );
   }
 }
