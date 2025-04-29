6dfc1e226bd16fa3b7fbf6e2e0e73039c1739312
Oscar <avilaoe@miamioh.edu>
2025-04-29 10:45:53 -0400

    app updates

--- App/lib/settings.dart
+++ App/lib/settings.dart
@@ -22,14 +22,17 @@ class SettingsState extends State<Settings> {
     _bleManager.writeData("shutdown".codeUnits);
   }
 
+  void _takePicture() {
+    _bleManager.writeData("takePicture".codeUnits);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ElevatedButton(
-          onPressed: _shutDown,
-          child: const Text("Shutdown SafeStep"),
-        ),
+        ElevatedButton(onPressed: _shutDown, child: Text("Shutdown SafeStep")),
+        ElevatedButton(onPressed: _takePicture, child: Text("Take picture")),
       ],
     );
   }
